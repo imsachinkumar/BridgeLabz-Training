a2c4b6ba96613f54601c2e743a5b40f29b02bb69
Sachin Kumar <sachinthakurela2005@gmail.com>
2026-08-09 11:51:44 +0530

    Added Day-5 GreetingsApp Crud Operation

--- Day-5/greetingsApp/greetingsApp/src/test/java/com/bridgelabz/greetingsApp/GreetingsAppApplicationTests.java
+++ Day-5/greetingsApp/greetingsApp/src/test/java/com/bridgelabz/greetingsApp/GreetingsAppApplicationTests.java
@@ -1,10 +1,10 @@
-package com.myGreetingApp;
+package com.bridgelabz.greetingsApp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class GreetingsAppApplicationTests {
 
 	@Test
 	void contextLoads() {
